f181152db985e4ada5efa487ef8b5cfac2cd2d4f
vicaragon <jiangxinran.ruc@gmail.com>
2015-07-14 08:27:05 -0700

    Initial Commit

---
+++ Solution/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Solution
+//
+//  Created by Xjiang on 7/14/15.
+//  Copyright (c) 2015 Xjiang. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
