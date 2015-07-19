d4fd298e36c25b43ba6f72482996f17e07a8c3f2
vicaragon <vjiang@vjiang-8460p.internal.synopsys.com>
2015-07-18 18:16:44 -0700

    add initial project

---
+++ Solution/Solution.cpp
@@ -0,0 +1,23 @@
+// Solution.cpp : Defines the entry point for the console application.
+//
+
+#include <stdlib.h>
+
+using namespace std;
+
+class LRUCache {
+
+public:
+	LRUCache(int Capacity) {
+
+	}
+
+	int get(int key) {
+		
+		return -1;
+	}
+
+	void set(int key, int value) {
+
+	}
+};
